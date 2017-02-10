d09de6a0f52114612e92ff98480ee123417befc2
somdari <matthewchoi7@gmail.com>
2017-02-10 11:10:56 +1100

    adding Header component and TeslaBattery container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 import TeslaBattery from './containers/TeslaBattery';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <TeslaBattery />    
       </div>
     );
--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -6,18 +6,6 @@ class TeslaBattery extends React.Component {
     return (
       <form className="tesla-battery">
         <h1>Range Per Charge</h1>
-        <div class="tesla-battery__notice">
-          <p>
-            The actual amount of range that you experience will vary based 
-            on your particular use conditions. See how particular use conditions 
-            may affect your range in our simulation model.
-          </p>
-          <p>
-            Vehicle range may vary depending on the vehicle configuration, 
-            battery age and condition, driving style and operating, environmental 
-            and climate conditions.
-          </p>
-        </div>
       </form>
     )
   }
