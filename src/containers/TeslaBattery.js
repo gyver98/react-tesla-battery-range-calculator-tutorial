5857a527317634a1b4c2985d049db7128d27c9d9
somdari <matthewchoi7@gmail.com>
2017-02-10 17:10:01 +1100

    adding TeslaStats

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './TeslaBattery.css';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 import TeslaCar from '../components/TeslaCar/TeslaCar';
+import TeslaStats from '../components/TeslaStats/TeslaStats';
+import { getModelData } from '../services/BatteryService';
 
 class TeslaBattery extends React.Component {
   constructor(props) {
     super(props);
+
+    this.calculateStats = this.calculateStats.bind(this);
+    this.statsUpdate = this.statsUpdate.bind(this);
 
     this.state = {
       carstats: [],
@@ -17,13 +22,38 @@ class TeslaBattery extends React.Component {
       }
     }
   }
+
+  calculateStats = (models, value) => {
+    const dataModels = getModelData();
+    return models.map(model => {
+      const { speed, temperature, climate, wheels } = value;
+      const miles = dataModels[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
+      return {
+        model,
+        miles
+      };
+    });
+  }
+
+  statsUpdate() {
+    const carModels = ['60', '60D', '75', '75D', '90D', 'P100D'];
+    // Fetch model info from BatteryService and calculate then update state
+    this.setState({
+      carstats: this.calculateStats(carModels, this.state.config)
+    })  
+  }
+
+  componentDidMount() {
+     this.statsUpdate(); 
+  }
   
   render() {
-    const { config } = this.state;
+    const { config, carstats } = this.state;
     return (
       <form className="tesla-battery">
         <h1>Range Per Charge</h1>
         <TeslaCar wheelsize={config.wheels}/>
+        <TeslaStats carstats={carstats}/>
         <TeslaNotice />
       </form>
     )
