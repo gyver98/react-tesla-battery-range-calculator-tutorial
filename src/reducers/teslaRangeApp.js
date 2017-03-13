50be29fbf8a2eb3c42896c61c1fc39e8f389b7a0
somdari <matthewchoi7@gmail.com>
2017-03-13 16:44:17 +1100

    update reducers

--- src/reducers/teslaRangeApp.js
+++ src/reducers/teslaRangeApp.js
@@ -1,12 +1,13 @@
+import { getModelData } from '../services/BatteryService';
 
 const initialState = {
-  carstats:[
-    {miles:246, model:"60"},
-    {miles:250, model:"60D"},
-    {miles:297, model:"75"},
-    {miles:306, model:"75D"},
-    {miles:336, model:"90D"},
-    {miles:376, model:"P100D"}
+  carstats: [
+    { miles: 246, model: "60" },
+    { miles: 250, model: "60D" },
+    { miles: 297, model: "75" },
+    { miles: 306, model: "75D" },
+    { miles: 336, model: "90D" },
+    { miles: 376, model: "P100D" }
   ],
   config: {
     speed: 55,
@@ -16,11 +17,43 @@ const initialState = {
   }
 }
 
+function updateStats(state, newState) {
+  return {
+    ...state,
+    config: newState.config,
+    carstats: calculateStats(newState)
+  }
+}
+
+function calculateStats(state) {
+  const models = ['60', '60D', '75', '75D', '90D', 'P100D'];
+  const dataModels = getModelData();
+  return models.map(model => {
+    const { speed, temperature, climate, wheels } = state.config;
+    const miles = dataModels[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
+    return {
+      model,
+      miles
+    };
+  });
+}
+
 function appReducer(state = initialState, action) {
   switch (action.type) {
-    
+    case 'CHANGE_CLIMATE': {
+      const newState = {
+        ...state,
+        config: {
+          climate: !state.config.climate,
+          speed: state.config.speed,
+          temperature: state.config.temperature,
+          wheels: state.config.wheels
+        }
+      };
+      return updateStats(state, newState);
+    }
     default:
-      return state 
+      return state
   }
 }
 
--- src/reducers/teslaRangeApp.spec.js
+++ src/reducers/teslaRangeApp.spec.js
@@ -17,11 +17,40 @@ const initialState =  {
   }
 }
 
+const climateChangeState = {
+  carstats:[
+    {miles:267, model:"60"},
+    {miles:273, model:"60D"},
+    {miles:323, model:"75"},
+    {miles:334, model:"75D"},
+    {miles:366, model:"90D"},
+    {miles:409, model:"P100D"}
+  ],
+  config: {
+    speed: 55,
+    temperature: 20,
+    climate: false,
+    wheels: 19
+  }
+}
+
 describe('test reducer', () => {
   it('should handle initial stat', () => {
     expect(
       appReducer(undefined, {})
     ).toEqual(initialState)
   })
+
+  it('should handle CHANGE_CLIMATE', () => {
+    expect(
+      appReducer(initialState,{
+        type: 'CHANGE_CLIMATE'
+      })
+    ).toEqual(climateChangeState)
+  })
 })
+
+
+  
+
 
