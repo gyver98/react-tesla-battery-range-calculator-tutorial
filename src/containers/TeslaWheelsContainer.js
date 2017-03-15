a9a6c93292ec011735f95ae0d7e19d00503beb79
somdari <matthewchoi7@gmail.com>
2017-03-15 20:34:57 +1100

    adding TeslaWheelsContainer

---
+++ src/containers/TeslaWheelsContainer.js
@@ -0,0 +1,21 @@
+import TeslaWheels from '../components/TeslaWheels';
+import { connect } from 'react-redux';
+import { changeWheel } from '../actions'
+
+const mapStateToProps = (state) => {
+  return {
+    value: state.config.wheels
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeHandler:(size) => {
+      dispatch(changeWheel(size))
+    }
+  }
+}
+
+const TeslaWheelsContainer = connect(mapStateToProps, mapDispatchToProps)(TeslaWheels)
+
+export default TeslaWheelsContainer;
