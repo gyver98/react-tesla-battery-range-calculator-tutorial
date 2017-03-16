69da4708efe731391f0e0c71519e8aef38193672
gyver98 <matthewchoi7@gmail.com>
2017-03-16 13:54:00 +1100

    adding Redux dev tool config

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header/Header';
 import TeslaBatteryContainer from './containers/TeslaBatteryContainer';
 import appReducer from './reducers/teslaRangeApp';
 
-const store = createStore(appReducer);
+const store = createStore(appReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 class App extends Component {
   render() {
