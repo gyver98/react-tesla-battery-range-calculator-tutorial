ea9cf8c6d091338fee3c1da53d91dad0a98bf98c
somdari <matthewchoi7@gmail.com>
2017-03-18 20:45:22 +1100

    part1 roll back

--- src/components/TeslaCounter/TeslaCounter.js
+++ src/components/TeslaCounter/TeslaCounter.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import './TeslaCounter.css';
 
-const TeslaCounter = ({ initValues, currentValue, increment, decrement }) => (
+const TeslaCounter = (props) => (
   <div className="tesla-counter">
-    <p className="tesla-counter__title">{initValues.title}</p>
+    <p className="tesla-counter__title">{props.initValues.title}</p>
     <div className="tesla-counter__container cf">
       <div className="tesla-counter__item">
         <p className="tesla-counter__number">
-          { currentValue }
-          <span>{ initValues.unit }</span>
+          { props.currentValue }
+          <span>{ props.initValues.unit }</span>
         </p>
         <div className="tesla-counter__controls">
           <button 
-            onClick={(e) => {
-              e.preventDefault();
-              increment(currentValue)}} 
-            disabled={currentValue >= initValues.max} 
+            onClick={(e) => props.increment(e, props.initValues.title)} 
+            disabled={props.currentValue >= props.initValues.max} 
           >
           </button>
           <button 
-            onClick={(e) => {
-              e.preventDefault();
-              decrement(currentValue)}} 
-            disabled={currentValue <= initValues.min} 
+            onClick={(e) => props.decrement(e, props.initValues.title)} 
+            disabled={props.currentValue <= props.initValues.min} 
           >
           </button>
         </div>
